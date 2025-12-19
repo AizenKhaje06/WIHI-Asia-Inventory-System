88af820ec3be1c2d124d68b3ecb030a0a73f6451
Aizen06 <jhakewihi072@gmail.com>
2025-12-20 00:06:27 +0800

    update

--- components/client-layout.tsx
+++ components/client-layout.tsx
@@ -48,7 +48,7 @@ export default function ClientLayout({
           <div
             className={`
               flex-shrink-0 flex h-screen flex-col border-r border-sidebar-border bg-gradient-dark text-white transition-all duration-300 ease-in-out
-              ${sidebarOpen ? (sidebarCollapsed ? 'w-20' : 'w-[260px]') : 'w-0 lg:w-20 lg:translate-x-0'}
+              ${sidebarOpen ? (sidebarCollapsed ? 'w-20' : 'w-full md:w-64') : 'w-0 lg:w-20 lg:translate-x-0'}
               ${sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
             `}
           >
